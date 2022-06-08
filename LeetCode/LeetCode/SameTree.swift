b89433c4ee5a6ea8265fe6a81329fd33b8a4296b
MarinaMamdouh <marina.mam.riad@gmail.com>
2022-06-08 16:46:15 +0200

    add iterative solution to sameTree problem

--- LeetCode/LeetCode/SameTree.swift
+++ LeetCode/LeetCode/SameTree.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 class SameTree{
-    func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+    func isSameTreeDivideConquer(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
         
         if p == nil && q == nil{
             return true
@@ -15,9 +15,33 @@ class SameTree{
         if p?.val != q?.val{
             return false
         }
-        let leftTreeIsSame = isSameTree(p?.left, q?.left)
-        let rightTreeIsSame = isSameTree(p?.right, q?.right)
+        let leftTreeIsSame = isSameTreeDivideConquer(p?.left, q?.left)
+        let rightTreeIsSame = isSameTreeDivideConquer(p?.right, q?.right)
         return leftTreeIsSame && rightTreeIsSame
         
+    }
+    
+    func isSameTreeIterative(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        var queue = [TreeNode?]()
+        queue.append(p)
+        queue.append(q)
+        while !queue.isEmpty{
+            let firstTree = queue.removeFirst()
+            let secondTree =  queue.removeFirst()
+            if firstTree == nil && secondTree == nil{
+                continue
+            }
+            
+            if firstTree?.val != secondTree?.val{
+                return false
+            }
+            
+            queue.append(firstTree?.left)
+            queue.append(secondTree?.left)
+            queue.append(firstTree?.right)
+            queue.append(secondTree?.right)
+        }
+        
+        return true
     }
 }
--- LeetCode/LeetCodeTests/SameTreeTests.swift
+++ LeetCode/LeetCodeTests/SameTreeTests.swift
@@ -19,8 +19,11 @@ class SameTreeTests: XCTestCase {
         tree2.left = TreeNode(2)
         tree2.right =  TreeNode(3)
         
-        let result = problem.isSameTree(tree1, tree2)
-        XCTAssertEqual(result, true)
+        let result1 = problem.isSameTreeDivideConquer(tree1, tree2)
+        XCTAssertEqual(result1, true)
+        
+        let result2 =  problem.isSameTreeIterative(tree1, tree2)
+        XCTAssertEqual(result2, true)
         
     
     }
@@ -30,8 +33,11 @@ class SameTreeTests: XCTestCase {
         tree1.left =  TreeNode(1)
         let tree2 = TreeNode(1)
         tree2.right = TreeNode(1)
-        let result = problem.isSameTree(tree1, tree2)
-        XCTAssertEqual(result, false)
+        let result1 = problem.isSameTreeDivideConquer(tree1, tree2)
+        XCTAssertEqual(result1, false)
+        
+        let result2 =  problem.isSameTreeIterative(tree1, tree2)
+        XCTAssertEqual(result2, false)
     }
     
     func testExample2() throws{
@@ -40,8 +46,11 @@ class SameTreeTests: XCTestCase {
         let tree2 = TreeNode(1)
         tree2.right = TreeNode(1)
         
-        let result = problem.isSameTree(tree1, tree2)
-        XCTAssertEqual(result, true)
+        let result1 = problem.isSameTreeDivideConquer(tree1, tree2)
+        XCTAssertEqual(result1, true)
+        
+        let result2 =  problem.isSameTreeIterative(tree1, tree2)
+        XCTAssertEqual(result2, true)
         
     }
 
