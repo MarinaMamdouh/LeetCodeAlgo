8e1f87bc5b475a5682029360364090a4dc3c4c6e
MarinaMamdouh <marina.mam.riad@gmail.com>
2022-06-09 20:07:44 +0200

    add square root problem solution and tests

--- LeetCode/LeetCode/sqrt(x).swift
+++ LeetCode/LeetCode/sqrt(x).swift
@@ -9,9 +9,28 @@ import Foundation
 
 class SquareRoot{
     func mySqrt(_ x: Int) -> Int {
-       // let halfNumber = Double(x) / 2.0
-       // 1 , 4 , 9
-       // let dict = ]
-        return -1
+        if x < 2{
+            return x
+        }
+        var start = 1
+        var end = x/2
+        var mid = 0
+        var double = 0
+        var previousMid = 0
+        while start <= end{
+            mid =  (start + (end - start) / 2);
+            double = mid * mid
+            if double == x {
+                return mid
+            }
+            if double < x {
+                previousMid = mid
+                start = mid + 1
+            }else{
+                end =  mid - 1
+            }
+        }
+        
+        return previousMid
     }
 }
--- LeetCode/LeetCodeTests/SquareRootTests.swift
+++ LeetCode/LeetCodeTests/SquareRootTests.swift
@@ -18,14 +18,22 @@ class SquareRootTests: XCTestCase {
     
     func testExample2() throws {
         let x = 8
-        let result = problem.mySqrt(x)
+        var result = problem.mySqrt(x)
+        XCTAssertEqual(result, 2)
+        
+        let y = 7
+        result = problem.mySqrt(y)
         XCTAssertEqual(result, 2)
     }
     
     func testExample3() throws {
-        let x = 8
-        let result = problem.mySqrt(x)
-        XCTAssertEqual(result, 2)
+        var x = 9
+        var result = problem.mySqrt(x)
+        XCTAssertEqual(result, 3)
+        
+         x = 11
+         result = problem.mySqrt(x)
+        XCTAssertEqual(result, 3)
     }
 
 
